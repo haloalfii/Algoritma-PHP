08126e7d8b618097e8fb2dcd20c7e92abccb63ba
haloalfii <alfiankurniawan85@gmail.com>
2021-10-07 23:12:52 +0700

    second commit, task number 6

--- soal-6.php
+++ soal-6.php
@@ -5,4 +5,28 @@ $arr = [
     ['r', 's', 't', 'u']
 ];
 
-echo $arr[0][2];
+$cari = 'fghi';
+$cari_split = str_split($cari);
+
+// echo $cari_split[0];
+$max = strlen($cari);
+
+// echo array_search($cari_split[0], array_column($arr, 0));
+
+for ($x = 0; $x < 4; $x++) {
+    for ($y = 0; $y < $max; $y++) {
+        if (array_search($cari_split[$y], array_column($arr, $x)) == NULL) {
+            echo array_search($cari_split[$y], array_column($arr, $x));
+            echo "<br>";
+        }
+        // else {
+        //     echo array_search($cari_split[$x], array_column($arr, $y));
+        // }
+    }
+}
+
+// if (array_search('s', array_column($arr, 1)) == NULL) {
+//     echo "ga ada";
+// } else {
+//     echo array_search('s', array_column($arr, 1));
+// }
